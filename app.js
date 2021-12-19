de30a19d1c7d50017e22c1dfbf0b88c11808cbc1
Daniel Powley <danpowley@gmail.com>
2021-12-19 18:14:53 +0000

    Basic Blackbox support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 var cors = require('cors')
 var bodyParser = require('body-parser')
 const NodeCache = require('node-cache')
+const moment = require('moment')
 
 const app = express()
 const port = 3000;
@@ -9,13 +10,131 @@ const port = 3000;
 app.use(cors()) // allow CORS requests
 app.use(bodyParser.json()) // parse application/json
 
-const appliedTeamsCache = new NodeCache({ stdTTL: 10 })
+const gameFinderAppliedCache = new NodeCache({ stdTTL: 10 })
 
-app.post('/coach/apply-teams', (req, res) => {
+let blackboxLastDrawDate = moment()
+const blackboxDrawFrequencyInMinutes = 1
+const blackboxAppliedCache = new NodeCache({ stdTTL: 3600 }) // only hold long enough till the draw is made
+const blackboxDrawCache = new NodeCache({ stdTTL: 7200 }) // hold for a couple of hours
+
+function getRandomInteger(max) {
+  return Math.floor(Math.random() * max)
+}
+
+function getRandomArrayElement(array) {
+  return array[getRandomInteger(array.length)]
+}
+
+setInterval(function () {
+  console.log('Making blackbox draw')
+
+  const blackboxDrawTeams = []
+  for (const coachName of blackboxAppliedCache.keys()) {
+    const blackboxApplication = blackboxAppliedCache.get(coachName)
+    if (blackboxApplication.teams.length > 0) {
+      blackboxDrawTeams.push({
+        coachName: coachName,
+        team: getRandomArrayElement(blackboxApplication.teams)
+      })
+    }
+  }
+
+  let newPair = true
+  let team1;
+  let team2;
+  const blackboxMatches = []
+  for (const coachSettings of blackboxDrawTeams) {
+    if (newPair) {
+      team1 = coachSettings
+      newPair = false
+    } else {
+      team2 = coachSettings
+
+      if (team1 && team2) {
+        blackboxMatches.push([team1, team2])
+      }
+
+      team1 = null
+      team2 = null
+      newPair = true
+    }
+  }
+
+  // only create a draw entry if a match up was made
+  if (blackboxMatches.length > 0) {
+    const drawKey = moment().format('YYYY-MM-DD-HH-mm')
+    blackboxDrawCache.set(drawKey, {drawKey: drawKey, date: new Date(), matches: blackboxMatches})
+  }
+
+  // reset the applied teams
+  blackboxAppliedCache.flushAll()
+
+  // record when the last draw took place, even if no-one was matched up
+  blackboxLastDrawDate = moment()
+
+}, blackboxDrawFrequencyInMinutes * 60000)
+
+app.post('/blackbox/apply', (req, res) => {
   const coach = req.body.coach
   const teams = req.body.teams
 
-  appliedTeamsCache.set(
+  blackboxAppliedCache.set(
+    coach.name,
+    {
+      coach: coach,
+      teams: teams
+    }
+  )
+
+  res.send({coachCount: blackboxAppliedCache.keys().length})
+})
+
+app.get('/blackbox/current', (req, res) => {
+  let coachCount = 0
+  let teamCount = 0
+  const timeOfNextDraw = blackboxLastDrawDate.clone().add(blackboxDrawFrequencyInMinutes, 'minutes');
+
+  for (const coachName of blackboxAppliedCache.keys()) {
+    const blackboxApplication = blackboxAppliedCache.get(coachName)
+    if (blackboxApplication.teams.length > 0) {
+      coachCount++
+    }
+    teamCount += blackboxApplication.teams.length
+  }
+
+  res.send({ coachCount: coachCount, teamCount: teamCount, timeOfNextDraw: timeOfNextDraw })
+})
+
+app.get('/blackbox/draw-results', (req, res) => {
+  const blackboxDrawCacheKeys = blackboxDrawCache.keys()
+
+  blackboxDrawCacheKeys.sort(function (a, b) {
+    if (a == b) {
+      return 0
+    } else if (a < b) {
+      return 1
+    } else {
+      return -1;
+    }
+  })
+
+  const results = []
+
+  for (const blackboxDrawCacheKey of blackboxDrawCacheKeys) {
+    const drawResult = blackboxDrawCache.get(blackboxDrawCacheKey)
+    if (drawResult !== undefined) {
+      results.push(drawResult)
+    }
+  }
+
+  res.send(results)
+})
+
+app.post('/game-finder/apply', (req, res) => {
+  const coach = req.body.coach
+  const teams = req.body.teams
+
+  gameFinderAppliedCache.set(
     coach.name,
     {
       coach: coach,
@@ -25,12 +144,12 @@ app.post('/coach/apply-teams', (req, res) => {
 
   let matchupData = {teams: [], coaches: []}
 
-  for (const oppCoachName of appliedTeamsCache.keys()) {
+  for (const oppCoachName of gameFinderAppliedCache.keys()) {
     if (oppCoachName === coach.name) {
       continue
     }
 
-    const oppCoachTeams = appliedTeamsCache.get(oppCoachName)
+    const oppCoachTeams = gameFinderAppliedCache.get(oppCoachName)
     if (oppCoachTeams === undefined) {
       continue
     }
